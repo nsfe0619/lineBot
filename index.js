5e0ce4cdd1365c2da776507950d7bd25621d7210
Isaac <nsfe0619@gmail.com>
2018-03-28 17:24:38 +0800

    測試api

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var bot = linebot({
   channelAccessToken: 'PUNfMYWJC2ARrSsLOfdxmmer7GCouKmb9LOwCe/KpBMbgjkWEGicxv7DzD38dL0dxzWFpjE3nk3XpyJ6qhrjvljczlO+7wpTq7oKjZTGUQAqiyuzac7vyXi7yK/7mbcaP/iewDMZJQwARRuNllrY7QdB04t89/1O/w1cDnyilFU='
 });
 
+var request=require('request');
+request('https://api.avgle.com/v1/categories',function(error,response,body){
+	console.log('error',error);
+	console.log('statusCode',response && resopnse.statusCode);
+	console.log('body:',body);
+})
+
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
     var msg = event.message.text;
@@ -29,24 +36,3 @@ var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
-
-function _japan() {
-  clearTimeout(timer2);
-  request({
-    url: "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm",
-    method: "GET"
-  }, function(error, response, body) {
-    if (error || !body) {
-      return;
-    } else {
-      var $ = cheerio.load(body);
-      var target = $(".rate-content-sight.text-right.print_hide");
-      console.log(target[15].children[0].data);
-      jp = target[15].children[0].data;
-      if (jp < 0.28) {
-        bot.push('使用者 ID', '現在日幣 ' + jp + '，該買啦！');
-      }
-      timer2 = setInterval(_japan, 120000);
-    }
-  });
-}
