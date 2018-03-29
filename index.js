54469004412b4a69718fdf93f2767e0f9c64b9a1
Isaac <nsfe0619@gmail.com>
2018-03-29 11:42:55 +0800

    no message

--- index.js
+++ index.js
@@ -5,30 +5,10 @@ var cheerio = require('cheerio');
 
 
 var request = require('request');
-request('https://www.ptt.cc/bbs/Beauty/index.html', function (error, response, body) {
-
-	var $=cheerio.load(body);
-	var beautyArr=[];
-	$('.r-ent .title a').each(function(i,elem){
-		beautyArr.push($('.r-ent .title a').eq(i).attr('href'));
-	})
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-	console.log(beautyArr);
-});
-
-function getImages(post, callback) {
-  request('https://www.ptt.cc' + post, (err, res, body) => {
-    var images = body.match(/imgur.com\/[0-9a-zA-Z]{7}/g);
-    images = [ ...new Set(images) ]
-    callback(images);
-  })
-}
 var bot = linebot({
   channelId: 1565375319,
-  channelSecret: 'bc407d7bba57dc515d022928cc361429',
-  channelAccessToken: 'PUNfMYWJC2ARrSsLOfdxmmer7GCouKmb9LOwCe/KpBMbgjkWEGicxv7DzD38dL0dxzWFpjE3nk3XpyJ6qhrjvljczlO+7wpTq7oKjZTGUQAqiyuzac7vyXi7yK/7mbcaP/iewDMZJQwARRuNllrY7QdB04t89/1O/w1cDnyilFU='
+  channelSecret: 'c58ae1cb241fbac4858f26fef7c94a9c',
+  channelAccessToken: 'SA9InT9GKvrYsJG9a4m4hAwdp7qZdOqSNBKb9AjTGf46Oyy8sswiljKMqA7dVT4hxzWFpjE3nk3XpyJ6qhrjvljczlO+7wpTq7oKjZTGUQDwpxd9NCrCGymo5D3DN53dXMP1pdMxleC2rS64I8o8lQdB04t89/1O/w1cDnyilFU='
 });
 
 bot.on('message', function(event) {
