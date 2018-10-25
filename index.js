149b2a51ccbd808fb427894edf39c922239cd33f
Isaac <nsfe0619@gmail.com>
2018-10-25 16:20:55 +0800

    no message

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ bot.on('message', function(event) {
 
 const app = express();
 const linebotParser = bot.parser();
-app.post('/index.html', linebotParser);
+app.post('/', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
